e6b5510eccfa9ad169e668a120ba91d3400d82fc
Glitch (common-shared-picture) <none>
2021-11-02 22:31:08 +0000

    👱🛳 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+$(".hide-button").click(function(){
+    $(".ghost-image").hide();
+});
 
+$(".show-button").click(function(){
+    $(".ghost-image").show();
+});
+
+$(".message-button").click(function(){
+    $(".message").text("Hello I am a ghost! Can you guess my name?");
+});
+
+$(".update-button").click(function(){
+    $(".ghost-image").css('height', '170px');});
